3c735c8effc9e571f6d0780356f4fe7a4cd450ac
kadsonlima <kadson-kaio@hotmail.com>
2022-04-01 15:51:27 -0300

    Atualização funções, falta implementar o evento fechar pedido

--- index.js
+++ index.js
@@ -1,59 +1,63 @@
-let as = document.querySelectorAll(".products> label");
-let button = document.querySelector(".button");
-
-function getpedido() {
-    let pratos = document.querySelectorAll(":checked")
-    let total = document.querySelector(".total")
-    let itens = document.querySelectorAll(".item");
-    let valor = 0;
-
-    for (let i = 0; i < 3; i++) {
-        let prato = pratos[i].parentElement.querySelectorAll("span");
-
-        itens[i].childNodes[1].innerText = prato[0].innerHTML.replace('R$ ', '');
-        itens[i].childNodes[3].innerText = prato[1].innerText.replace('R$ ', '');
-        valor = valor + parseFloat((prato[1].innerText).replace('R$ ', ''));
-        total.childNodes[1].innerText = "R$ " + valor.toFixed(2);
-    }
- 
-
-}
 
 // FUDEU DE VEZ
 
-
-as.forEach(function (e) {
-    e.addEventListener("click", () => {
-        let selecionados = document.querySelectorAll(".products > label > :checked");
-        console.log("teste")
-        if (selecionados.length == 3) {
-
-            document.querySelector(".button")
-                .addEventListener("click", () => {
-                    document.querySelector(".closed-demand").style.visibility = "visible"
-                    getpedido();
-                })
-            document.querySelector(".cancel")
-                .addEventListener("click", () => {
-                    document.querySelector(".closed-demand").style.visibility = "hidden"
-                })
-
-            button.style.backgroundColor = "green";
-            button.innerText = "Fechar Pedido"
-            button.style.cursor = "pointer"
-        } else {
-            button.style.backgroundColor = "#CBCBCB";
-            button.innerText = "Selecione os 3 itens para fechar o pedido char Pedido"
-            button.style.cursor = "initial"
+let pratos = document.querySelectorAll(".produto.pratos");
+pratos.forEach(clickado =>{
+    clickado.addEventListener("click", ()=>{
+        let marcado = document.querySelector(".pratos.marcado");
+        if (marcado !== null && marcado !== clickado) {
+            marcado.classList.toggle("marcado");
+            clickado.classList.toggle("marcado")
+        }else{
+            clickado.classList.toggle("marcado")
         }
-
-    });
+        //
+        verificar();
+    })
 });
 
-document.querySelector(".pedir").addEventListener("click",(e)=>{
+let bebida = document.querySelectorAll(".produto.bebida");
+bebida.forEach(clickado =>{
+    clickado.addEventListener("click", ()=>{
+        let marcado = document.querySelector(".bebida.marcado");
+        if (marcado !== null && marcado !== clickado) {
+            marcado.classList.toggle("marcado");
+            clickado.classList.toggle("marcado")
+        }else{
+            clickado.classList.toggle("marcado")
+        }
+        //
+        verificar();
+    })
+});
+let sobremesa = document.querySelectorAll(".produto.sobremesa");
+sobremesa.forEach(clickado =>{
+    clickado.addEventListener("click", ()=>{
+        let marcado = document.querySelector(".sobremesa.marcado");
+        if (marcado !== null && marcado !== clickado) {
+            marcado.classList.toggle("marcado");
+            clickado.classList.toggle("marcado")
+        }else{
+            clickado.classList.toggle("marcado")
+        }
+        //
+        verificar();
 
-    whatsApp('caju', 'suco', 'puos', valor.toFixed(2));
-})
+    })
+});
+
+function verificar(){
+    let produtos = document.querySelectorAll(".marcado")
+    let button = document.querySelector(".button")
+    if(produtos.length == 3){
+        button.classList.add("disponivel")
+        button.innerText = "Fechar Pedido"
+    }else{
+        button.innerText = "Selecione os 3 itens para fechar o pedido"
+        button.classList.remove("disponivel")
+    }
+    
+}
 
 function whatsApp(prato, bebida, sobremesa, valor) {
     let nome = prompt("Digite seu nome :");
@@ -66,6 +70,10 @@ function whatsApp(prato, bebida, sobremesa, valor) {
     Nome: ${nome}
     Endereço: ${rua}`
 
-
+    whatsApp('caju', 'suco', 'puos', valor.toFixed(2));
     window.open(link)
 }
+
+/*
+            
+*/
